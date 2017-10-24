0f87f44b9dc49c60ce0f09b2996f0cc8446c9cc3
Eric Nilsson <CaterHatterPillar@gmail.com>
2017-10-24 04:50:01 +0200

    Remove internal and implicit variable blacklists. Comparatively few internal and implicit variables end up in the graph (presumably because few interact with each other). Not so sure it is worth the trouble to exclude these. Most noise is caused by user variables anyway.

--- make_graph.py
+++ make_graph.py
@@ -5,7 +5,7 @@ from argparse import ArgumentParser, FileType
 from collections import defaultdict
 from graphviz import Digraph
 
-def relations(database, blacklist):
+def relations(database):
     assignments = defaultdict(set)
 
     re_assignment = re.compile('^(\w+) :?= .*$')
@@ -19,21 +19,13 @@ def relations(database, blacklist):
             continue
 
         assignee = match_assignee.group(1)
-        if assignee in blacklist:
-            continue
-
         for match_variable in re.finditer(re_variable, line):
-            variable = match_variable.group(1)
-            if variable not in blacklist:
-                assignments[assignee].add(variable)
+            assignments[assignee].add(match_variable.group(1))
 
     return assignments
 
 def make_graph(database, graph, view):
-    with open('internal.vars') as internal:
-        internal = internal.readlines()
-
-    assignments = relations(database, internal)
+    assignments = relations(database)
 
     nodes = {assignee for (assignee, _) in assignments.iteritems()}
     [nodes.update(variables) for (_, variables) in assignments.iteritems()]
@@ -46,7 +38,7 @@ def make_graph(database, graph, view):
     dot.render(graph, view = view)
 
 if __name__ == "__main__":
-    parser = ArgumentParser('make_graph.py')
+    parser = ArgumentParser(__file__)
     parser.add_argument('--database', type = FileType('r'),
                         help = ("GNU Make database filename; if no filename is"
                                 " provided the database is expected on the"
@@ -57,7 +49,6 @@ if __name__ == "__main__":
                         help = "Don't open the assembled graph")
 
     args = vars(parser.parse_args())
-
     database = args['database'] if args['database'] else sys.stdin
     make_graph(database, args['graph'], args['view'])
 
