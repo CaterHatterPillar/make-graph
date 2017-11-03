7fe4df28e18887df92bfafdb04a6315bd8b6beaa
Eric Nilsson <CaterHatterPillar@gmail.com>
2017-11-03 17:25:59 +0100

    Provide an option to include nodes without any edges

--- make_graph.py
+++ make_graph.py
@@ -24,8 +24,23 @@ def relations(database):
 
     return assignments
 
-def make_graph(database, graph, view):
-    assignments = relations(database)
+def trim(assignments, include_single):
+    if include_single:
+        return assignments
+
+    # not assigned other variables
+    singles = {assignee for (assignee, variables) in
+               assignments.iteritems() if len(variables) == 0}
+    # and not assigned to another variables
+    [singles.difference_update(variables) for (_, variables) in
+     assignments.iteritems()]
+
+    [assignments.pop(single, None) for single in singles]
+
+    return assignments
+
+def make_graph(database, graph, include_single, view):
+    assignments = trim(relations(database), include_single)
 
     nodes = {assignee for (assignee, _) in assignments.iteritems()}
     [nodes.update(variables) for (_, variables) in assignments.iteritems()]
@@ -46,11 +61,13 @@ if __name__ == "__main__":
     parser.add_argument('--graph', default = 'graph',
                         help = ("Graph name; defaults to 'graph'"))
     parser.add_argument('--include-single', action = 'store_true',
-                        help = "Include lone nodes")
+                        help = "Include nodes without edges")
+    parser.add_argument('--no-view', dest = 'view', action = 'store_false',
+                        help = "Don't open the assembled graph")
 
     args = vars(parser.parse_args())
     database = args['database'] if args['database'] else sys.stdin
-    make_graph(database, args['graph'], args['view'])
+    make_graph(database, args['graph'], args['include_single'], args['view'])
 
     if database != sys.stdin:
         database.close()
